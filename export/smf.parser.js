a67e864d35e6742a960cdb5cb23834b397ffe086
logue <logue@hotmail.co.jp>
2013-10-11 18:41:03 +0900

    Initial Commit

--- export/smf.parser.js
+++
@@ -1,9 +0,0 @@
-goog.require('SMF.Parser');
-
-/** @define {boolean} */
-var SMF_PARSER_EXPORT = false;
-
-if (SMF_PARSER_EXPORT) {
-  goog.exportSymbol('SMF.Parser', SMF.Parser);
-  goog.exportSymbol('SMF.Parser.prototype.parse', SMF.Parser.prototype.parse);
-}
