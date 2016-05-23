18f013ab938593ffb6645d1e7c2c964837796bbe
logue <logue@hotmail.co.jp>
2016-05-23 22:13:48 +0900

    workerのエクスポートのコミットし忘れ

---
+++ export/smf.worker.js
@@ -0,0 +1,22 @@
+goog.require('SMF.Worker');
+
+/** @define {boolean} */
+var SMF_WORKER_EXPORT = false;
+
+if (SMF_WORKER_EXPORT) {
+  goog.exportSymbol('SMF.Worker', SMF.Worker);
+  goog.exportSymbol('SMF.Worker.prototype.play', SMF.Worker.prototype.play);
+  goog.exportSymbol('SMF.Worker.prototype.stop', SMF.Worker.prototype.stop);
+  goog.exportSymbol('SMF.Worker.prototype.loadMidiFile', SMF.Worker.prototype.loadMidiFile);
+  goog.exportSymbol('SMF.Worker.prototype.loadMldFile', SMF.Worker.prototype.loadMldFile);
+  goog.exportSymbol('SMF.Worker.prototype.setLoop', SMF.Worker.prototype.setLoop);
+  goog.exportSymbol('SMF.Worker.prototype.setCC111Loop', SMF.Worker.prototype.setCC111Loop);
+  goog.exportSymbol('SMF.Worker.prototype.setFalcomLoop', SMF.Worker.prototype.setFalcomLoop);
+  goog.exportSymbol('SMF.Worker.prototype.setMFiLoop', SMF.Worker.prototype.setMFiLoop);
+  goog.exportSymbol('SMF.Worker.prototype.setWebMidiLink', SMF.Worker.prototype.setWebMidiLink);
+  goog.exportSymbol('SMF.Worker.prototype.getWebMidiLink', SMF.Worker.prototype.getWebMidiLink);
+  goog.exportSymbol('SMF.Worker.prototype.setTempoRate', SMF.Worker.prototype.setTempoRate);
+  goog.exportSymbol('SMF.Worker.prototype.setMasterVolume', SMF.Worker.prototype.setMasterVolume);
+  goog.exportSymbol('SMF.Worker.prototype.getCopyright', SMF.Worker.prototype.getCopyright);
+  goog.exportSymbol('SMF.Worker.prototype.getSequenceName', SMF.Worker.prototype.getSequenceName);
+}
