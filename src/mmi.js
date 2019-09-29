743940847b6f36c4d384e122d52833db96df6ad9
logue <logue@hotmail.co.jp>
2019-09-29 11:54:09 +0900

    マルチトラックのMabiIccoファイルに対応。 TODO: ・コーラスが常に一番最初のトラックのコーラス指定の楽器になる。 ・和音が出せない（打楽器、シロフォンなど）

--- src/mmi.js
+++ src/mmi.js
@@ -10,6 +10,23 @@ import { MetaEvent, ChannelEvent, SystemExclusiveEvent } from './midi_event';
  */
 export default class MabiIcco extends MakiMabiSequence {
   /**
+   * @param {ByteArray} input
+   * @param {Object=} optParams
+   */
+  constructor(input, optParams = {}) {
+    super(input, optParams);
+    /** @type {array} */
+    this.input = String.fromCharCode.apply('', new Uint16Array(input)).split(/\r\n|\r|\n/);
+    /** @type {Array.<Array.<Object>>} 全トラックの演奏情報 */
+    this.tracks = [];
+    /** @type {Array.<Array.<Uint8Array>>} WMLに送る生のMIDIイベント */
+    this.plainTracks = [];
+    /** @param {number} トラック数 */
+    this.numberOfTracks = 1;
+    /** @type {number} 分解能 */
+    this.timeDivision = optParams.timeDivision || 96;
+  }
+  /**
    * パース処理
    */
   parse() {
@@ -23,17 +40,48 @@ export default class MabiIcco extends MakiMabiSequence {
   parseHeader() {
     /** @type {TextEncoder} */
     this.encoder = new TextEncoder('utf-8');
+
+    /** @type {array} 各トラックごと複数存在する変数名 */
+    const multipleKeys = ['mml-track', 'name', 'program', 'songProgram', 'panpot'];
+    const ret = {};
+    /** @type {number} */
+    let trackNo = -1;
+    ret.track = [];
+
+    for (let i = 0; i < this.input.length; i++) {
+      const line = this.input[i].trim();
+      if (i === 0) {
+        if (line !== '[mml-score]') {
+          throw new Error('Not MabiIcco File.');
+        }
+        continue;
+      }
+      const [key, value] = line.split('=');
+      if (multipleKeys.includes(key)) {
+        if (key === 'mml-track') {
+          trackNo++;
+          ret.track[trackNo] = {};
+          // -が含まれる名前を変数名として使うと面倒なので・・・。
+          ret.track[trackNo].mml = value;
+        } else {
+          ret.track[trackNo][key] = (key === 'name') ? value : value | 0;
+        }
+      } else {
+        ret[key] = value;
+      }
+    }
     /** @param {string} タイトル */
-    this.title = this.input['mml-score'].title;
+    this.title = ret.title;
     /** @param {string} 著者情報 */
-    this.author = this.input['mml-score'].author;
-    /** @param {number} 解像度 */
-    this.timeDivision = 96;
-    // 拍子記号
-    const timeSig = this.input['mml-score'].author.split('/');
-
-    // TODO: 合奏対応のmmiファイルの処理
-
+    this.author = ret.author;
+    /** @param {array}  */
+    const mmiTempo = (ret.tempo !== '') ? ret.tempo.split('T') : [384, 120];
+    /** @param {number} 分解能（MabiIccoの場合、テンポの項目に含まれている？） */
+    this.timeDivision = mmiTempo[0] | 0 / 4;
+    /** @param {number} テンポ */
+    this.tempo = mmiTempo[1] | 0;
+    /** @param {array} 拍子記号 */
+    const timeSig = ret.time.split('/');
     /** @type {array}  */
     const headerTrack = [];
     // GM Reset
@@ -42,69 +90,60 @@ export default class MabiIcco extends MakiMabiSequence {
     headerTrack.push(new MetaEvent('SequenceTrackName', 0, 0, [this.title]));
     headerTrack.push(new MetaEvent('CopyrightNotice', 0, 0, [this.author]));
     headerTrack.push(new MetaEvent('TimeSignature', 0, 0, [timeSig[0] | 0 || 4, timeSig[1] | 0 || 4, 0, 0]));
+    headerTrack.push(new MetaEvent('SetTempo', 0, 0, [Math.floor(60000000 / this.tempo)]));
     headerTrack.push(new MetaEvent('EndOfTrack', 0, 0));
     this.tracks.push(headerTrack);
 
-    // infomationおよびmms-fileを取り除く
-    delete this.input['mml-score'].author;
-    delete this.input['mml-score'].version;
-    delete this.input['mml-score'].title;
-    delete this.input['mml-score'].time;
-    delete this.input['mml-score'].tempo;
+    this.input = ret.track;
   };
   /**
    * MML parse
    */
   parseTracks() {
-    const input = this.input;
     /** @type {array} MIDIイベント */
     let track = [];
     /** @type {array} 終了時間比較用 */
     const endTimes = [];
-    /** @type {number} チャンネル */
-    let ch = 0;
 
-    for (const item in input) {
-      if (input.hasOwnProperty(item)) {
-        if (!input[item]['mml-track'].match(/^(?:MML@)(.*)/gm)) {
+    for (let ch = 0; ch < this.input.length; ch++) {
+      const current = this.input[ch];
+      if (!current.mml.match(/^(?:MML@)(.*)/gm)) {
+        continue;
+      }
+
+      /** @param {array} MMLの配列（簡易マッチ） */
+      const mmls = RegExp.$1.split(',');
+
+      // 楽器名
+      track.push(new MetaEvent('InsturumentName', 0, 48, [current.name]));
+      // プログラムチェンジ
+      track.push(new ChannelEvent('ProgramChange', 0, 96, ch, current.program));
+      if (current.songProgram !== -1) {
+        // コーラス用
+        track.push(new ChannelEvent('ProgramChange', 0, 112, 15, current.songProgram));
+      }
+      // パン(CC:0x10)
+      track.push(new ChannelEvent('ControlChange', 0, 154, ch, 10, current.panpot));
+
+      // MMLの各チャンネルの処理
+      for (let chord = 0; chord < current.mml.length; chord++) {
+        if (chord === 3 && current.songProgram !== -1) {
+          // ch 16はコーラス用
+          ch = 15;
+        }
+        if (mmls[chord] === void 0) {
           continue;
         }
 
-        /** @param {array} MMLの配列（簡易マッチ） */
-        const mmls = RegExp.$1.split(',');
-
-        // 楽器名
-        track.push(new MetaEvent('InsturumentName', 0, 48, input[item]['name']));
-        // プログラムチェンジ
-        track.push(new ChannelEvent('ProgramChange', 0, 96, ch, input[item]['program'] | 0));
-        if (input[item]['songProgram'] !== -1) {
-          // コーラス用
-          track.push(new ChannelEvent('ProgramChange', 0, 112, 15, input[item]['songProgram'] | 0));
-        }
-        // パン(CC:0x10)
-        track.push(new ChannelEvent('ControlChange', 0, 154, ch, 10, input[item]['panpot'] | 0));
-
-        // MMLの各チャンネルの処理
-        for (let chord = 0; chord < mmls.length; chord++) {
-          if (chord === 3 && input[item]['songProgram'] !== -1) {
-            // ch 16はコーラス用
-            ch = 15;
-          }
-          if (mmls[chord] === void 0) {
-            continue;
-          }
-
-          /** @param {PSGConverter} */
-          const mml2Midi = new PSGConverter({ timeDivision: this.timeDivision, channel: ch, timeOffset: 386, mml: mmls[chord] });
-          // トラックにマージ
-          track = track.concat(mml2Midi.events);
-          endTimes.push(mml2Midi.endTime);
-        }
-        ch++;
-        // トラック終了
-        track.concat(new MetaEvent('EndOfTrack', 0, Math.max(endTimes)));
-        this.tracks.push(track);
+        /** @param {PSGConverter} */
+        const mml2Midi = new PSGConverter({ timeDivision: this.timeDivision, channel: ch, timeOffset: 386, mml: mmls[chord] });
+        // トラックにマージ
+        track = track.concat(mml2Midi.events);
+        endTimes.push(mml2Midi.endTime);
       }
+      // トラック終了
+      track.concat(new MetaEvent('EndOfTrack', 0, Math.max(endTimes)));
+      this.tracks.push(track);
     }
     this.numberOfTracks = this.tracks.length;
   }
