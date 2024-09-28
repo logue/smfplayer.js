5a0dcd1d89eb51bd4f67371690fa9d34e9a4e1a8
Logue <logue@hotmail.co.jp>
2024-09-29 02:36:48 +0900

    Migrate to pnpm. Update dependencies.

--- eslint.config.js
+++ eslint.config.js
@@ -24,6 +24,7 @@ export default [
       'docs/',
       'src/**/*.generated.*',
       'eslint.config.js',
+      'pnpm-locl.yml',
     ],
   },
   { languageOptions: { globals: { ...globals.browser, ...globals.node } } },
